a5ad454089d00d8b88124f25fa03c4ba4b6cf9a2
cassidy <cassidykovell@Cassidys-iMac.lan>
2024-05-23 22:23:14 -0700

    fixing issue for render

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect('mongodb+srv://cassidy0kovell:Billmaloney4ever!@cluster0.n33evcs.mongodb.net/googlebooks?retryWrites=true&w=majority&appName=Cluster0');
 
 module.exports = mongoose.connection;
